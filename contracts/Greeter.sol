a96705d57c31d8ef1084796dd4962d359f77fac9
kevinourg <kevinjrg@gmail.com>
2021-08-03 19:39:54 +0200

    Add files via upload

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -3,15 +3,14 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-
 contract ChainShot {
   uint x;
 
   constructor(uint _x) {
-    x = _x;
-  }
+      x = _x;
+    }
 
-  function modifyvar() public view returns (uint) {
-    return x << 1;
+  function quadruple() public view returns (uint) {
+    return x << 2;
   }
 }
